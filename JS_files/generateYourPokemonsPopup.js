4a342ac06ed234bea1d18b91498ee34f3e72e888
Malvy Delaban <delaban.malvy@gmail.com>
2025-03-14 15:27:02 +0100

    [ADD] sort by type and num my poke

--- JS_files/generateYourPokemonsPopup.js
+++ JS_files/generateYourPokemonsPopup.js
@@ -98,10 +98,131 @@ function fillYourPokemonsPopup(pokemonList) {
     }
 }
 
+function fillYourPokemonsPopupByType(pokemonList) {
+    let temp_sorted = owned_pokemons;
+    temp_sorted = temp_sorted.sort(function (a, b) {
+        return pokedex[a.pokedexId].type.name.localeCompare(pokedex[b.pokedexId].type.name);
+    });
+
+    for (let i = 0; i < Object.keys(temp_sorted).length; i++) {
+        // Créer un Pokémon unique à afficher
+        const singlePokemon = document.createElement('div');
+        singlePokemon.classList.add('popup-content-single-pokemon');
+
+        // Partie gauche avec l'image et le type
+        const leftSide = document.createElement('div');
+        leftSide.classList.add('popup-content-left-side-by-type');
+
+        leftSide.addEventListener('click', () => {
+            generatePokemonDetailPopup(owned_pokemons[i]);
+        });
+
+        const typeChip = createTypeChipPopupYourPokemons(pokedex[owned_pokemons[i].pokedexId]);
+
+        const rarityBackground = document.createElement('img');
+        rarityBackground.src = pokedex[temp_sorted[i].pokedexId].rarity.n_sprite ? pokedex[temp_sorted[i].pokedexId].rarity.n_sprite : pokedex[temp_sorted[i].pokedexId].rarity.sprite;
+        rarityBackground.alt = 'rarity background';
+        rarityBackground.classList.add('popup-content-rarity-background');
+
+        const pokemonImage = document.createElement('img');
+        pokemonImage.src = temp_sorted[i].sprite;
+        pokemonImage.alt = temp_sorted[i].name;
+        pokemonImage.classList.add('popup-content-pokemon-image');
+
+        leftSide.appendChild(rarityBackground);
+        leftSide.appendChild(pokemonImage);
+        leftSide.appendChild(typeChip);
+        pokemonList.appendChild(leftSide);
+    }
+}
+
+function fillYourPokemonsPopupByPokedex(pokemonList) {
+    let temp_sorted = owned_pokemons;
+    temp_sorted = temp_sorted.sort(function (a, b) {
+        return pokedex[a.pokedexId].id - pokedex[b.pokedexId].id;
+    });
+
+    for (let i = 0; i < Object.keys(temp_sorted).length; i++) {
+        // Créer un Pokémon unique à afficher
+        const singlePokemon = document.createElement('div');
+        singlePokemon.classList.add('popup-content-single-pokemon');
+
+        // Partie gauche avec l'image et le type
+        const leftSide = document.createElement('div');
+        leftSide.classList.add('popup-content-left-side-by-type');
+
+        leftSide.addEventListener('click', () => {
+            generatePokemonDetailPopup(owned_pokemons[i]);
+        });
+
+        const typeChip = createTypeChipPopupYourPokemons(pokedex[owned_pokemons[i].pokedexId]);
+
+        const rarityBackground = document.createElement('img');
+        rarityBackground.src = pokedex[temp_sorted[i].pokedexId].rarity.n_sprite ? pokedex[temp_sorted[i].pokedexId].rarity.n_sprite : pokedex[temp_sorted[i].pokedexId].rarity.sprite;
+        rarityBackground.alt = 'rarity background';
+        rarityBackground.classList.add('popup-content-rarity-background');
+
+        const pokemonImage = document.createElement('img');
+        pokemonImage.src = temp_sorted[i].sprite;
+        pokemonImage.alt = temp_sorted[i].name;
+        pokemonImage.classList.add('popup-content-pokemon-image');
+
+        leftSide.appendChild(rarityBackground);
+        leftSide.appendChild(pokemonImage);
+        leftSide.appendChild(typeChip);
+        pokemonList.appendChild(leftSide);
+    }
+}
+
+function removeElementsByClass(className) {
+    const elements = document.getElementsByClassName(className);
+    while (elements.length > 0) {
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
+
+function DisplayByType(pokemonListWrapper) {
+    removeElementsByClass("popup-content-your-pokemons-list");
+    removeElementsByClass("popup-content-your-pokemons-list-by-type");
+
+    const pokemonList = document.createElement('div');
+    pokemonList.classList.add('popup-content-your-pokemons-list-by-type');
+    fillYourPokemonsPopupByType(pokemonList);
+    pokemonListWrapper.appendChild(pokemonList);
+}
+
+function DisplayByPokedex(pokemonListWrapper) {
+    removeElementsByClass("popup-content-your-pokemons-list");
+    removeElementsByClass("popup-content-your-pokemons-list-by-type");
+
+    const pokemonList = document.createElement('div');
+    pokemonList.classList.add('popup-content-your-pokemons-list-by-type');
+    fillYourPokemonsPopupByPokedex(pokemonList);
+    pokemonListWrapper.appendChild(pokemonList);
+}
+
+function DisplayBaseDisplay(pokemonListWrapper) {
+    owned_pokemons.sort((a, b) => {
+        if (b.isInTeam !== a.isInTeam)
+            return b.isInTeam - a.isInTeam;
+        return b.level - a.level;
+    });
+    removeElementsByClass("popup-content-your-pokemons-list");
+    removeElementsByClass("popup-content-your-pokemons-list-by-type");
+    const pokemonList = document.createElement('div');
+    pokemonList.classList.add('popup-content-your-pokemons-list');
+
+    fillYourPokemonsPopup(pokemonList);
+    addListenerYourPokemonsPopup(pokemonList);
+
+    pokemonListWrapper.appendChild(pokemonList);
+}
+
 function generateYourPokemonsPopup() {
+    let currentDisplayState = 0;
     document.body.style.overflow = 'hidden'; // Désactive le scroll
     owned_pokemons.sort((a, b) => b.isInTeam - a.isInTeam);
-    
+
     // Créer les éléments principaux
     const popupBackgroundContainer = document.createElement('div');
     popupBackgroundContainer.classList.add('popup-background');
@@ -118,7 +239,7 @@ function generateYourPokemonsPopup() {
 
     const title = document.createElement('p');
     title.textContent = 'Mes Pokémons (' + Object.keys(owned_pokemons).length + ')';
-    
+
     const closeButton = document.createElement('img');
     closeButton.src = 'sprites/misc/cross_icon.png';
     closeButton.style.cursor = 'pointer';
@@ -133,20 +254,63 @@ function generateYourPokemonsPopup() {
     titleLine.appendChild(title);
     titleLine.appendChild(closeButton);
 
+    const displaySwitch = document.createElement('div');
+    displaySwitch.classList.add('popup-content-display-switch');
+
     // Wrapper de la liste des Pokémon
     const pokemonListWrapper = document.createElement('div');
     pokemonListWrapper.classList.add('popup-content-wrapper');
+
+    const baseSwitch = document.createElement('div');
+    baseSwitch.classList.add('popup-content-display-switch-button', 'popup-content-display-switch-button-selected');
+    baseSwitch.textContent = "Défaut";
+    displaySwitch.appendChild(baseSwitch);
+    baseSwitch.addEventListener('click', () => {
+        if (currentDisplayState == 0)
+            return;
+        currentDisplayState = 0;
+        byType.classList.remove("popup-content-display-switch-button-selected");
+        ByPokedex.classList.remove("popup-content-display-switch-button-selected");
+        baseSwitch.classList.add("popup-content-display-switch-button-selected");
+        DisplayBaseDisplay(pokemonListWrapper);
+    });
+
+    const byType = document.createElement('div');
+    byType.classList.add('popup-content-display-switch-button');
+    byType.textContent = "Type";
+    displaySwitch.appendChild(byType);
+    byType.addEventListener('click', () => {
+        if (currentDisplayState == 1)
+            return;
+        currentDisplayState = 1;
+        baseSwitch.classList.remove("popup-content-display-switch-button-selected");
+        ByPokedex.classList.remove("popup-content-display-switch-button-selected");
+        byType.classList.add("popup-content-display-switch-button-selected");
+        DisplayByType(pokemonListWrapper);
+    });
+
+    const ByPokedex = document.createElement('div');
+    ByPokedex.classList.add('popup-content-display-switch-button');
+    ByPokedex.textContent = "Num.";
+    displaySwitch.appendChild(ByPokedex);
+    ByPokedex.addEventListener('click', () => {
+        if (currentDisplayState == 2)
+            return;
+        currentDisplayState = 2;
+        byType.classList.remove("popup-content-display-switch-button-selected");
+        baseSwitch.classList.remove("popup-content-display-switch-button-selected");
+        ByPokedex.classList.add("popup-content-display-switch-button-selected");
+        DisplayByPokedex(pokemonListWrapper);
+    });
+
     titleLine.appendChild(pokemonListWrapper);
 
     // Contenu de la liste des Pokémon
-    const pokemonList = document.createElement('div');
-    pokemonList.classList.add('popup-content-your-pokemons-list');
 
-    fillYourPokemonsPopup(pokemonList);
-    addListenerYourPokemonsPopup(pokemonList);
+    DisplayBaseDisplay(pokemonListWrapper);
 
-    pokemonListWrapper.appendChild(pokemonList);
     popupContent.appendChild(titleLine);
+    popupContent.appendChild(displaySwitch);
     popupContent.appendChild(pokemonListWrapper);
     popupContainer.appendChild(popupContent);
     popupBackgroundContainer.appendChild(popupContainer);
